43113b505e50035f1b76cd737a3eac98a9fee58c
Alex Zverianskii <alex@zverianskii.com>
2020-12-03 17:23:04 +0300

    release 3.0

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     name='textvec',
     use_scm_version=True,
     description='Supervised text features extraction',
-    version='2.0',
-    url='https://github.com/zveryansky/textvec',
-    author='Alex Zveryansky',
-    author_email='',
+    version='3.0',
+    url='https://github.com/textvec/textvec',
+    author='Alex Zverianskii',
+    author_email='alex@zverianskii.com',
     license='MIT',
     classifiers=[
     'License :: OSI Approved :: MIT License',
-
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.2',
@@ -20,7 +19,9 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7'
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
     ],
     keywords='text nlp vectorization scikit-learn',
     packages=find_packages(exclude=['examples']),
